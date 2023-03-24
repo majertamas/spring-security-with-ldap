83214dce84733098d76aa69ccdc46d2f1fb4abe4
majer.tamas <majer.tamas@mikrum.hu>
2023-03-24 18:03:06 +0100

    first commit

---
+++ src/main/java/hu/mikrum/springsecuritywithldap/SpringSecurityWithLdapApplication.java
@@ -0,0 +1,13 @@
+package hu.mikrum.springsecuritywithldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithLdapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityWithLdapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/mikrum/springsecuritywithldap/SpringSecurityWithLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.mikrum.springsecuritywithldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithLdapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
